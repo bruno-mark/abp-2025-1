d3ef9f309ce07bb2b0df6f0e6725312831ca15d6
bruno-mark <mark-bruno98@hotmail.com>
2025-04-14 01:05:39 -0300

    feature(integrationBD-JS):inegration professores finish crud

--- backend/integracaoback--bd/integracaoProfessores.js
+++ backend/integracaoback--bd/integracaoProfessores.js
@@ -32,3 +32,62 @@ inserirProfessores(1, "Arley Souza", "arley.souza@fatec.sp.gov.br");
 inserirProfessores(2, "Antonio Egydio", "antonio.egydio@fatec.sp.gov.br");
 inserirProfessores(3, "Marcelo Sudo", "marcelo.sudo@fatec.sp.gov.br" );
 inserirProfessores(4, "Henrique", 'henrique@fatec.sp.gov.br');
+
+async function consultarProfessores() {
+    try {
+        const res = await client.query(
+            'SELECT * FROM professor',
+        );
+        console.log("Professores:", res.rows);
+    } catch (err) {
+        console.error("Erro ao buscar Professores:", err);
+    }
+}
+
+async function alterarIdProfessores(id, id_novo) {
+    try {
+        const res = await client.query(
+            'UPDATE professor SET id_professor = $2 WHERE id_professor = $1 RETURNING *',
+            [id, id_novo]
+        );
+        console.log("Professor alterado:", res.rows[0]);
+    } catch (err) {
+        console.error("Erro ao alterar Professor:", err);
+    }
+}
+
+async function alterarNomeProfessores(nome, nome_novo) {
+    try {
+        const res = await client.query(
+            'UPDATE professor SET nome_professor = $2 WHERE nome_professor = $1 RETURNING *',
+            [nome, nome_novo]
+        );
+        console.log("Professor alterado:", res.rows[0]);
+    } catch (err) {
+        console.error("Erro ao alterar Professor:", err);
+    }
+}
+
+async function alterarEmailProfessores(email, email_novo) {
+    try {
+        const res = await client.query(
+            'UPDATE professor SET email_professor = $2 WHERE email_professor = $1 RETURNING *',
+            [email, email_novo]
+        );
+        console.log("Professor alterado:", res.rows[0]);
+    } catch (err) {
+        console.error("Erro ao alterar Professor:", err);
+    }
+}
+
+async function deletarProfessores(id) {
+    try {
+        const res = await client.query(
+            'DELETE FROM professor WHERE id_professor = $1 RETURNING *',
+            [id]
+        );
+        console.log("Professor alterado:", res.rows[0]);
+    } catch (err) {
+        console.error("Erro ao alterar Professor:", err);
+    }
+}
