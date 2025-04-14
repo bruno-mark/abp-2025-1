f3f97d353aae8585116e7e8d937ed508ad3e96ec
bruno-mark <mark-bruno98@hotmail.com>
2025-04-14 01:05:24 -0300

    feature(integrationBD-JS):inegration professor_turma finish crud

--- backend/integracaoback--bd/integracaoProfessor_turma.js
+++ backend/integracaoback--bd/integracaoProfessor_turma.js
@@ -35,3 +35,54 @@ async function inserirAmbiente(idTurma, idProfessor) {
    inserirAmbiente(1, 2);
    inserirAmbiente(1, 3);
    inserirAmbiente(1, 4);
+
+   async function consultarAmbiente() {
+    try{
+        const res = await client.query(
+        'SELECT * FROM turma_professor'
+    );
+    console.log('Turma_professor:', res.rows);
+    } 
+    catch (err) {
+        console.error('Erro ao inserir turma_professor:', err);
+    }
+    }
+
+    async function alterarIdTurmaAmbiente(idTurma, idTurmaNovo) {
+        try{
+            const res = await client.query(
+            'UPDATE turma_professor SET id_turma = $1 WHERE id_turma = $2 RETURNING *',
+            [idTurmaNovo, idTurma]
+        );
+        console.log('Turma_professor alterado:', res.rows[0]);
+        } 
+        catch (err) {
+            console.error('Erro ao alterar turma_professor:', err);
+        }
+    }
+
+    async function alterarIdProfessorAmbiente(idProfessor, idProfessorNovo) {
+        try{
+            const res = await client.query(
+            'UPDATE turma_professor SET id_professor = $1 WHERE id_professor = $2 RETURNING *',
+            [idProfessorNovo, idProfessor]
+        );
+        console.log('Turma_professor alterado:', res.rows[0]);
+        } 
+        catch (err) {
+            console.error('Erro ao alterar turma_professor:', err);
+        }
+    }
+
+    async function deletarAmbiente(idTurma) {
+        try{
+            const res = await client.query(
+            'DELETE FROM turma_professor WHERE id_turma = $1 RETURNING *',
+            [idTurma]
+        );
+        console.log('Turma_professor deletado:', res.rows[0]);
+        } 
+        catch (err) {
+            console.error('Erro ao deletar turma_professor:', err);
+        }
+    }
