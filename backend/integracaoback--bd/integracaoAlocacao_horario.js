7760a92999c92583a4cf9572bf1a9bb3d42d25a5
bruno-mark <mark-bruno98@hotmail.com>
2025-04-14 01:02:14 -0300

    feature(integrationBD-JS):inegration alocacao_horario finish crud

--- backend/integracaoback--bd/integracaoAlocacao_horario.js
+++ backend/integracaoback--bd/integracaoAlocacao_horario.js
@@ -56,4 +56,65 @@ client.connect()
    inserirAlocacao_horario(24, 4);
    inserirAlocacao_horario(25, 4);
    
+   async function consultarAlocacao_horario() {
+    try{
+        const res = await client.query(
+        'SELECT * FROM alocacao_horario'
+    );
+    console.log('Alocações de horario:', res.rows);
+    } 
+    catch (err) {
+        console.error('Erro ao buscar alocação de horário:', err);
+    }
+   } 
 
+   consultarAlocacao_horario();
+
+   async function alterarIdAlocacao_horario(idAlocacao, idAlocacaoNovo) {
+    try{
+        const res = await client.query(
+            'UPDATE alocacao_horario SET id_alocacao = $2 WHERE id_alocacao = $1 RETURNING *',
+        [idAlocacao, idAlocacaoNovo]
+
+
+    );
+    console.log('Alocação de horario alterada:', res.rows[0]);
+    } 
+    catch (err) {
+        console.error('Erro ao alterar alocação de horário:', err);
+    }
+   }
+
+   async function alterarIdProfessorAlocacao_horario(idProfessor, idProfessorNovo) {
+    try{
+        const res = await client.query(
+            'UPDATE alocacao_horario SET id_professor = $4 WHERE id_alocacao = $1 AND id_professor = $2 RETURNING *',
+        [idProfessor, idProfessorNovo]
+
+
+    );
+    console.log('Alocação de horario alterada:', res.rows[0]);
+    } 
+    catch (err) {
+        console.error('Erro ao alterar alocação de horário:', err);
+    }
+   }
+
+   //alterarAlocacao_horario(1, 2, 3, 4);
+
+   async function deletarAlocacao_horario(idAlocacao) {
+    try{
+        const res = await client.query(
+            'DELETE FROM alocacao_horario WHERE idAlocacao = $1 RETURNING *',
+        [idAlocacao]
+
+
+    );
+    console.log('Alocação de horario inserida:', res.rows[0]);
+    } 
+    catch (err) {
+        console.error('Erro ao inserir alocação de horário:', err);
+    }
+   }
+
+    //deletarAlocacao_horario(1);
