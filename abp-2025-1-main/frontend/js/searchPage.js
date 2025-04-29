9154c08f2bc3b601feea50bd7fe4ceb9e529aadf
Márcio Roberto Bueno Júnior <marcinbueno01@hotmail.com>
2025-04-29 19:37:57 -0300

    Add files via upload

---
+++ abp-2025-1-main/frontend/js/searchPage.js
@@ -0,0 +1,20 @@
+function toggleListaCursos() {
+    const lista = document.getElementById('minhaLista');
+    lista.style.display = lista.style.display === 'none' ? 'block' : 'none';
+  }
+
+  function toggleListaHorariosDSM() {
+    const lista = document.getElementById('horariosDsm');
+    lista.style.display = lista.style.display === 'none' ? 'block' : 'none';
+  }
+
+  function toggleListaHorariosGEO() {
+    const lista = document.getElementById('horariosGeo');
+    lista.style.display = lista.style.display === 'none' ? 'block' : 'none';
+  }
+
+  function toggleListaHorariosMA() {
+    const lista = document.getElementById('horariosMA');
+    lista.style.display = lista.style.display === 'none' ? 'block' : 'none';
+  }
+
